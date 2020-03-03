e9d775d25b7b9f312f507353f8c52315e58bf4a1
DianaBanana <dianaskybin@gmail.com>
2020-03-03 00:41:19 -0500

    Initial commit

---
+++ Change Calculator.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace _200421248A1
+{
+    partial class ChangeCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Total = new System.Windows.Forms.Label();
+            this.Paid = new System.Windows.Forms.Label();
+            this.Change = new System.Windows.Forms.Label();
+            this.Toonies = new System.Windows.Forms.Label();
+            this.Loonies = new System.Windows.Forms.Label();
+            this.Quaters = new System.Windows.Forms.Label();
+            this.Dimes = new System.Windows.Forms.Label();
+            this.Nickels = new System.Windows.Forms.Label();
+            this.txtPaid = new System.Windows.Forms.TextBox();
+            this.txtChange = new System.Windows.Forms.TextBox();
+            this.txtToonies = new System.Windows.Forms.TextBox();
+            this.txtLoonies = new System.Windows.Forms.TextBox();
+            this.txtQuaters = new System.Windows.Forms.TextBox();
+            this.txtDimes = new System.Windows.Forms.TextBox();
+            this.txtNickels = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Total
+            // 
+            this.Total.AutoSize = true;
+            this.Total.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.575F);
+            this.Total.Location = new System.Drawing.Point(7, 15);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(60, 25);
+            this.Total.TabIndex = 0;
+            this.Total.Text = "Total";
+            // 
+            // Paid
+            // 
+            this.Paid.AutoSize = true;
+            this.Paid.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.575F);
+            this.Paid.Location = new System.Drawing.Point(120, 15);
+            this.Paid.Name = "Paid";
+            this.Paid.Size = new System.Drawing.Size(55, 25);
+            this.Paid.TabIndex = 1;
+            this.Paid.Text = "Paid";
+            // 
+            // Change
+            // 
+            this.Change.AutoSize = true;
+            this.Change.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.575F);
+            this.Change.Location = new System.Drawing.Point(239, 15);
+            this.Change.Name = "Change";
+            this.Change.Size = new System.Drawing.Size(87, 25);
+            this.Change.TabIndex = 2;
+            this.Change.Text = "Change";
+            // 
+            // Toonies
+            // 
+            this.Toonies.AutoSize = true;
+            this.Toonies.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.575F);
+            this.Toonies.Location = new System.Drawing.Point(7, 108);
+            this.Toonies.Name = "Toonies";
+            this.Toonies.Size = new System.Drawing.Size(89, 25);
+            this.Toonies.TabIndex = 3;
+            this.Toonies.Text = "Toonies";
+            // 
+            // Loonies
+            // 
+            this.Loonies.AutoSize = true;
+            this.Loonies.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.575F);
+            this.Loonies.Location = new System.Drawing.Point(120, 108);
+            this.Loonies.Name = "Loonies";
+            this.Loonies.Size = new System.Drawing.Size(88, 25);
+            this.Loonies.TabIndex = 4;
+            this.Loonies.Text = "Loonies";
+            // 
+            // Quaters
+            // 
+            this.Quaters.AutoSize = true;
+            this.Quaters.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.575F);
+            this.Quaters.Location = new System.Drawing.Point(231, 108);
+            this.Quaters.Name = "Quaters";
+            this.Quaters.Size = new System.Drawing.Size(88, 25);
+            this.Quaters.TabIndex = 5;
+            this.Quaters.Text = "Quaters";
+            // 
+            // Dimes
+            // 
+            this.Dimes.AutoSize = true;
+            this.Dimes.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.575F);
+            this.Dimes.Location = new System.Drawing.Point(345, 108);
+            this.Dimes.Name = "Dimes";
+            this.Dimes.Size = new System.Drawing.Size(72, 25);
+            this.Dimes.TabIndex = 6;
+            this.Dimes.Text = "Dimes";
+            // 
+            // Nickels
+            // 
+            this.Nickels.AutoSize = true;
+            this.Nickels.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.575F);
+            this.Nickels.Location = new System.Drawing.Point(460, 108);
+            this.Nickels.Name = "Nickels";
+            this.Nickels.Size = new System.Drawing.Size(82, 25);
+            this.Nickels.TabIndex = 7;
+            this.Nickels.Text = "Nickels";
+            // 
+            // txtPaid
+            // 
+            this.txtPaid.Location = new System.Drawing.Point(123, 47);
+            this.txtPaid.Multiline = true;
+            this.txtPaid.Name = "txtPaid";
+            this.txtPaid.Size = new System.Drawing.Size(100, 31);
+            this.txtPaid.TabIndex = 8;
+            // 
+            // txtChange
+            // 
+            this.txtChange.Enabled = false;
+            this.txtChange.Location = new System.Drawing.Point(236, 47);
+            this.txtChange.Multiline = true;
+            this.txtChange.Name = "txtChange";
+            this.txtChange.Size = new System.Drawing.Size(102, 31);
+            this.txtChange.TabIndex = 9;
+            // 
+            // txtToonies
+            // 
+            this.txtToonies.Enabled = false;
+            this.txtToonies.Location = new System.Drawing.Point(12, 140);
+            this.txtToonies.Multiline = true;
+            this.txtToonies.Name = "txtToonies";
+            this.txtToonies.Size = new System.Drawing.Size(100, 31);
+            this.txtToonies.TabIndex = 10;
+            // 
+            // txtLoonies
+            // 
+            this.txtLoonies.Enabled = false;
+            this.txtLoonies.Location = new System.Drawing.Point(125, 140);
+            this.txtLoonies.Multiline = true;
+            this.txtLoonies.Name = "txtLoonies";
+            this.txtLoonies.Size = new System.Drawing.Size(98, 31);
+            this.txtLoonies.TabIndex = 11;
+            // 
+            // txtQuaters
+            // 
+            this.txtQuaters.Enabled = false;
+            this.txtQuaters.Location = new System.Drawing.Point(236, 140);
+            this.txtQuaters.Multiline = true;
+            this.txtQuaters.Name = "txtQuaters";
+            this.txtQuaters.Size = new System.Drawing.Size(102, 31);
+            this.txtQuaters.TabIndex = 12;
+            // 
+            // txtDimes
+            // 
+            this.txtDimes.Enabled = false;
+            this.txtDimes.Location = new System.Drawing.Point(350, 140);
+            this.txtDimes.Multiline = true;
+            this.txtDimes.Name = "txtDimes";
+            this.txtDimes.Size = new System.Drawing.Size(102, 31);
+            this.txtDimes.TabIndex = 13;
+            // 
+            // txtNickels
+            // 
+            this.txtNickels.Enabled = false;
+            this.txtNickels.Location = new System.Drawing.Point(463, 140);
+            this.txtNickels.Multiline = true;
+            this.txtNickels.Name = "txtNickels";
+            this.txtNickels.Size = new System.Drawing.Size(100, 31);
+            this.txtNickels.TabIndex = 14;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(12, 47);
+            this.txtTotal.MinimumSize = new System.Drawing.Size(100, 25);
+            this.txtTotal.Multiline = true;
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 31);
+            this.txtTotal.TabIndex = 15;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(23, 212);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(163, 50);
+            this.Calculate.TabIndex = 16;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(403, 212);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(160, 50);
+            this.Clear.TabIndex = 17;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // ChangeCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 287);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtNickels);
+            this.Controls.Add(this.txtDimes);
+            this.Controls.Add(this.txtQuaters);
+            this.Controls.Add(this.txtLoonies);
+            this.Controls.Add(this.txtToonies);
+            this.Controls.Add(this.txtChange);
+            this.Controls.Add(this.txtPaid);
+            this.Controls.Add(this.Nickels);
+            this.Controls.Add(this.Dimes);
+            this.Controls.Add(this.Quaters);
+            this.Controls.Add(this.Loonies);
+            this.Controls.Add(this.Toonies);
+            this.Controls.Add(this.Change);
+            this.Controls.Add(this.Paid);
+            this.Controls.Add(this.Total);
+            this.Name = "ChangeCalculator";
+            this.Text = "Change Calculator";
+            this.Load += new System.EventHandler(this.ChangeCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Total;
+        private System.Windows.Forms.Label Paid;
+        private System.Windows.Forms.Label Change;
+        private System.Windows.Forms.Label Toonies;
+        private System.Windows.Forms.Label Loonies;
+        private System.Windows.Forms.Label Quaters;
+        private System.Windows.Forms.Label Dimes;
+        private System.Windows.Forms.Label Nickels;
+        private System.Windows.Forms.TextBox txtPaid;
+        private System.Windows.Forms.TextBox txtChange;
+        private System.Windows.Forms.TextBox txtToonies;
+        private System.Windows.Forms.TextBox txtLoonies;
+        private System.Windows.Forms.TextBox txtQuaters;
+        private System.Windows.Forms.TextBox txtDimes;
+        private System.Windows.Forms.TextBox txtNickels;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ Change Calculator.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace _200421248A1
+{
+    public partial class ChangeCalculator : Form
+    {
+        public ChangeCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeCalculator_Load(object sender, EventArgs e)
+        {
+            // Making fileds unavailible for user to change
+            txtChange.Enabled = false;
+            txtToonies.Enabled = false;
+            txtLoonies.Enabled = false;
+            txtQuaters.Enabled = false;
+            txtDimes.Enabled = false;
+            txtNickels.Enabled = false;
+
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            float paid;
+            float total;
+            float change;
+            //Reading input and saving it as a string
+            String inputPaid = txtPaid.Text;
+            String inputTotal = txtTotal.Text;
+
+            /// <summary>
+            ///  Validate input by using simple "if" and "esle if" statments to make sure the format of the input is right, 
+            ///  if validation had passed go to "else" where calculations are 
+            /// </summary>
+
+            /// Making sure the input type in sumeric
+            if (!Single.TryParse(inputPaid, out paid) | !Single.TryParse(inputTotal, out total))
+            {
+                // If not, show an error massage in message box
+                MessageBox.Show("Invalid Paid or/and Total value", "Error");
+                // Clean calculations in a case if form already been used before and there something left
+                DeleteCalculations();
+            }
+
+            /// Validate if the total amount is a positive integer
+            else if (total < 0)
+            {
+                // If not, show an error massage in message box
+                MessageBox.Show("Total amount, must be positive integer", "Total amount Error");
+                // Clean calculations in a case if form already been used before and there something left
+                DeleteCalculations();
+            }
+
+            /// Validate if the paid amount is a positive integer
+            else if (paid <= 0)
+            {
+                // If not, show an error massage in message box
+                MessageBox.Show("Paid amount, must be positive integer", "Paid amount Error");
+                // Clean calculations in a case if form already been used before and there something left
+                DeleteCalculations();
+            }
+
+            ///Validating if the total amount less then paid.
+            ///If user had a paid amount error before, so paid variable has not been set, what is by default 0, total>0 will be True always
+            else if (total > paid && paid != 0 && total != 0)
+            {
+                // If not, show an error massage in message box
+                MessageBox.Show("Your payment is insufficient to cover the total, you still need to pay: " + (total - paid) + " dollars", "Error");
+                // Clean calculations in a case if form already been used before and there something left
+                DeleteCalculations();
+            }
+
+            /// All validations had passed
+            else
+            {
+                /// <summary>
+                /// Logic of the calculations:
+                /// To find out the number of the particular coins in every case: Take the floor from change (or remainder
+                /// from the previous calculation) devided by the coefficient by this particullar coin.
+                /// For example, if for a tonie the coefficient will be 2
+                /// </summary>
+
+                // Change textBox
+                change = paid - total;
+                //formate change value as currency and dispay
+                txtChange.Text = change.ToString("C", CultureInfo.CurrentCulture);
+
+                // Calculating number of tonies and dispaying it in textBox
+                decimal tonies = (decimal)Math.Floor(change / 2);
+                // saving rest in a remainder var
+                float remainder = change - ((float)tonies * 2);
+                //dispaying to the textbox
+                txtToonies.Text = tonies.ToString();
+
+                // Calculating number of loonies and dispaying it in textBox
+                decimal loonies = (decimal)Math.Floor(remainder / 1);
+                // saving rest in a remainder var
+                remainder = remainder - ((float)loonies * 1);
+                //dispaying to the textbox
+                txtLoonies.Text = loonies.ToString();
+
+                //Calculating number of quaters and dispaying it in textBox
+                int quaters = (int)(remainder / 0.25);
+                // saving rest in a remain var
+                double remain = remainder - ((float)quaters * 0.25);
+                //dispaying to the textbox
+                txtQuaters.Text = quaters.ToString();
+
+                //Calculating number of dimes and dispaying it in textBox
+                int dimes = (int)(remain / 0.10);
+                // saving rest in a remain var
+                remain = remain - ((float)dimes * 0.10);
+                //dispaying to the textbox
+                txtDimes.Text = dimes.ToString();
+
+                //Calculating number of dimes and dispaying it in textBox
+                int nickels = (int)(remain / 0.05);
+                // saving rest in a remain var
+                remain = remain - ((float)nickels * 0.05);
+                //where pennies get rounded down if there are 2c or less, and up to a nickel if 3 or more cents remain.
+                if (remain >= 0.03) { nickels = nickels + 1; }
+                //dispaying to the textbox
+                txtNickels.Text = nickels.ToString();
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            DeleteAll();
+        }
+
+        /// <summary>
+        ///  DeleteAll function cleaning all textboxes
+        /// </summary>
+        public void DeleteAll()
+        {
+            txtTotal.Clear();
+            txtPaid.Clear();
+            txtChange.Clear();
+            txtLoonies.Clear();
+            txtToonies.Clear();
+            txtQuaters.Clear();
+            txtDimes.Clear();
+            txtNickels.Clear();
+        }
+
+        /// <summary>
+        /// DeleteCalculations function cleaning calculation(unenabled) textboxes
+        /// </summary>
+        public void DeleteCalculations()
+        {
+            txtChange.Clear();
+            txtLoonies.Clear();
+            txtToonies.Clear();
+            txtQuaters.Clear();
+            txtDimes.Clear();
+            txtNickels.Clear();
+        }
+
+    }
+}
